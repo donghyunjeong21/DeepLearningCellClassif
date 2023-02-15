5c738bba5019406aa12a6097ae3c0b76a039025f
donghyunjeong21 <34069123+donghyunjeong21@users.noreply.github.com>
2023-02-15 16:24:14 -0500

    Update main_driver.py

--- main_driver.py
+++ main_driver.py
@@ -1,7 +1,7 @@
 import train_reader
 import generate_model
 import argparse
-import pathlib
+import os
 
 print('Running a test version of the Deep Learning Cell Classifier')
 
@@ -14,17 +14,14 @@ parser.add_argument('--sgmt', type = int, required = True)
 parser.add_argument('--use_GPU', type = bool, required = True)
 parser.add_argument('--diameter', type = int, required = True)
 parser.add_argument('--threshold', type = float, required = True)
-
-
-
 args = parser.parse_args()
 
-def main(args):
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-    # data_x, data_y = train_reader.run_pipeline(img_dir, 'tif', 0, 1, -1, True, 20, 2000)
+def main(args):
     data_x, data_y = train_reader.run_pipeline(args.path, args.file_type, args.input, args.truth, args.sgmt, args.use_GPU, args.diameter, args.threshold)
-    x = generate_model.init_model(40)
-    generate_model.fit_model(x, data_x, data_y, 40)
+    x = generate_model.init_model(args.diameter*2, args.threshold)
+    generate_model.fit_model(x, data_x, data_y, args.diameter*2)
     print(args)
 
 if __name__ == '__main__':
