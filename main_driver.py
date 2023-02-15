4f38124bb47402b7ffd23919a3bd5e611a5e9488
donghyunjeong21 <34069123+donghyunjeong21@users.noreply.github.com>
2023-02-15 16:49:52 -0500

    Update main_driver.py

--- main_driver.py
+++ main_driver.py
@@ -14,6 +14,7 @@ parser.add_argument('--sgmt', type = int, required = True)
 parser.add_argument('--use_GPU', type = bool, required = True)
 parser.add_argument('--diameter', type = int, required = True)
 parser.add_argument('--threshold', type = float, required = True)
+parser.add_argument('--modelName', type = str, required = False, default = 'untitled')
 args = parser.parse_args()
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -21,8 +22,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 def main(args):
     data_x, data_y = segment.run_pipeline(args.path, args.file_type, args.input, args.truth, args.sgmt, args.use_GPU, args.diameter, args.threshold)
     x = generate_model.init_model(args.diameter*2, args.threshold)
-    generate_model.fit_model(x, data_x, data_y, args.diameter*2)
-    print(args)
+    generate_model.fit_model(x, data_x, data_y, args.diameter*2, args.modelName)
+    
 
 if __name__ == '__main__':
     main(args)
