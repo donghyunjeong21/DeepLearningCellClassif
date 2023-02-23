f6a44d5aeb101861e3594810e814fccc326ca9ac
donghyunjeong21 <34069123+donghyunjeong21@users.noreply.github.com>
2023-02-23 14:17:06 -0500

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,78 @@
+
+from cellpose import models
+from cellpose import plot
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import pathlib
+from skimage import io
+import segment
+from tensorflow import keras
+
+def generate_input_for_pred(path_str, file_type, input_ch, segmt_ch):
+    images_lst = [file for file in os.listdir(path_str)
+                    if file.endswith(file_type)]
+    bf_imgs = []
+    sg_imgs = []
+    for filenames in images_lst:
+        im = io.imread(os.path.join(path_str,filenames))
+        im = dimension_compat(im)
+        bf_imgs.append(im[:,:,input_ch])
+        if segmt_ch != -1:
+            sg_imgs.append(im[:,:,segmt_ch])
+    return bf_imgs, sg_imgs
+
+
+def dimension_compat(im):
+    shape = im.shape
+    if shape[0] < shape[2]:
+        print('Your image looks like channel is listed first. Transposing...')
+        im = np.transpose(im, axes = (1,2,0))
+    return im
+
+def generate_segments(bf_img, sg_img = 0, use_GPU = False, diameter = 20):
+    model = models.Cellpose(gpu=use_GPU, model_type = 'cyto')
+    segment_bf = []
+
+    if sg_img != []:
+        segment_img = sg_img
+    else:
+        segment_img = bf_img
+    mask, flows, styles, diams = model.eval(segment_img, diameter=diameter, flow_threshold=0.6, channels=[0,0])
+
+    maxnum_cell = mask.max()
+    sgmt_dim = 2
+    bf_img = add_padding(bf_img, sgmt_dim*diameter)
+
+    for j in range(1, maxnum_cell):
+        x,y = np.where(mask==j)
+        bin_mask = add_padding(np.where(mask ==j, 1, 0), sgmt_dim*diameter)
+        xmin = x.min()
+        ymin = y.min()
+        segment_bf.append(np.multiply(bf_img[xmin:xmin+diameter*2,ymin:ymin+diameter*2], bin_mask[xmin:xmin+diameter*2,ymin:ymin+diameter*2]))
+    return segment_bf, mask
+def add_padding(im, d):
+    xdim, ydim = im.shape
+    leftpad = np.zeros([xdim, d])
+    y = np.append(im, leftpad, axis = 1)
+    xdim, ydim = y.shape
+    bottompad = np.zeros([d, ydim])
+    z = np.append(y, bottompad, axis = 0)
+    return z
+def run_model(segment_bf, model):
+    chosen_model = keras.models.load_model(model)
+    output = chosen_model.predict(segment_bf)
+    return output
+
+def run_pipeline(path_str, file_type, model_name, input_ch, segmt_ch, diam, preproc, useGPU):
+    bf_imgs, sg_imgs = generate_input_for_pred(path_str, file_type, input_ch, segmt_ch)
+    masks = []
+    labels = []
+    for i in range(len(bf_imgs)):
+        segment_bf, mask = generate_segments(bf_imgs[i], sg_imgs[i], use_GPU = useGPU, diameter = diam)
+        masks.append(mask)
+        segment_bf = np.stack(segment_bf,axis=0)
+        segment_bf = segment_bf.reshape(-1, diam*2, diam*2, 1)
+        labels.append(run_model(segment_bf, model_name))
+
+    return labels, masks
