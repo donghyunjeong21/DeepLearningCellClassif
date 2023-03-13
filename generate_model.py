33cb38fdf552a7159d4dc0f47624374c048bb75e
donghyunjeong21 <34069123+donghyunjeong21@users.noreply.github.com>
2023-03-13 17:28:45 -0400

    Update generate_model.py

--- generate_model.py
+++ generate_model.py
@@ -1,7 +1,8 @@
 import numpy as np
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import datasets, layers, models, optimizers
 from sklearn.model_selection import train_test_split
 from keras.regularizers import l2
+import tensorflow as tf
 
 def init_model(input_size, threshold):
     if threshold == -1:
@@ -11,38 +12,29 @@ def init_model(input_size, threshold):
     else:
         loss_type = 'binary_crossentropy'
         metric_type = 'accuracy'
-        fin_activation = 'sigmoid'
+        fin_activation = 'softmax'
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=(input_size, input_size, 1)))
     model.add(layers.Conv2D(128, kernel_size = (2,2), activation='relu'))
     model.add(layers.MaxPooling2D(pool_size = 2))
-    model.add(layers.Conv2D(64, kernel_size = (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D(pool_size = 2))
-    model.add(layers.Conv2D(32, kernel_size = (3,3), activation='relu'))
+    model.add(layers.Conv2D(64, kernel_size = (4,4), activation='relu'))
     model.add(layers.GlobalMaxPooling2D())
-    model.add(layers.Dense(64, activation = 'relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(64, activation = 'relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(32))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(16, activation = 'relu', kernel_regularizer=l2(0.01), bias_regularizer=l2(0.01)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(1, activation = fin_activation))
-
-    model.compile(optimizer = 'adam', loss = loss_type, metrics = [metric_type])
+    model.add(layers.Dense(64, activation = 'relu'))
+    model.add(layers.Dense(16, activation = 'relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(2, activation = fin_activation))
+    opt = optimizers.Adam(learning_rate=0.0000001)
+    model.compile(optimizer = opt, loss = loss_type, metrics = [metric_type])
 
     return model
 
 def fit_model(model, data_x, data_y, input_size, modelName):
-    train_x, test_x, train_y, test_y = data_org(data_x, data_y, input_size)
-    history = model.fit(train_x, train_y, batch_size = 100, epochs = 20, shuffle = True, validation_data = (test_x, test_y))
+    x,y = data_org(data_x, data_y, input_size)
+    history = model.fit(x, y, batch_size = 64, epochs = 50, shuffle = True, validation_split=0.2)
     model.save(modelName + '.h5')
     print('Model has been saved in the supplied directory as ' + modelName + '.h5')
 
 def data_org(data_x, data_y, size_selection):
     data_x = np.stack(data_x,axis=0)
-    train_x, test_x, train_y, test_y = train_test_split(data_x,data_y,test_size = 0.2)
-    train_x = train_x.reshape(-1, size_selection, size_selection, 1)
-    test_x = test_x.reshape(-1, size_selection, size_selection, 1)
-    return train_x, test_x, train_y, test_y
+    x = data_x.reshape(-1, size_selection, size_selection, 1)
+    return x,data_y
