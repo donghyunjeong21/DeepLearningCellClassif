53405193132cf0311e4736fed964588cb0489ec7
donghyunjeong21 <34069123+donghyunjeong21@users.noreply.github.com>
2023-04-03 12:29:20 -0400

    Update generate_model.py

--- generate_model.py
+++ generate_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from tensorflow.keras import datasets, layers, models, optimizers
+from tensorflow.keras import datasets, layers, models, optimizers, initializers, callbacks
 from sklearn.model_selection import train_test_split
 from keras.regularizers import l2
 import tensorflow as tf
@@ -12,26 +12,35 @@ def init_model(input_size, threshold):
     else:
         loss_type = 'binary_crossentropy'
         metric_type = 'accuracy'
-        fin_activation = 'softmax'
+        fin_activation = 'sigmoid'
+
+    initializer = initializers.RandomNormal(mean=0., stddev=1.)
+
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=(input_size, input_size, 1)))
-    model.add(layers.Conv2D(128, kernel_size = (2,2), activation='relu'))
+    model.add(layers.Conv2D(512, kernel_size = (3,3), activation='relu'))
     model.add(layers.MaxPooling2D(pool_size = 2))
-    model.add(layers.Conv2D(64, kernel_size = (4,4), activation='relu'))
+    model.add(layers.Conv2D(256, kernel_size = (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size = 2))
+    model.add(layers.Conv2D(128, kernel_size = (5,5), activation='relu'))
     model.add(layers.GlobalMaxPooling2D())
-    model.add(layers.Dense(64, activation = 'relu'))
-    model.add(layers.Dense(16, activation = 'relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(2, activation = fin_activation))
-    opt = optimizers.Adam(learning_rate=0.0000001)
+    # model.add(layers.Dense(256, kernel_initializer=initializer, activation = 'relu'))
+    # model.add(layers.Dropout(0.2))
+    # model.add(layers.Dense(128, kernel_initializer=initializer, activation = 'relu'))
+    model.add(layers.Dense(64, kernel_initializer=initializer, activation = 'relu'))
+    model.add(layers.Dense(32, kernel_initializer=initializer, activation = 'relu'))
+    model.add(layers.Dense(1, activation = fin_activation))
+    opt = optimizers.Adam(learning_rate=0.00001)
     model.compile(optimizer = opt, loss = loss_type, metrics = [metric_type])
 
     return model
 
 def fit_model(model, data_x, data_y, input_size, modelName):
     x,y = data_org(data_x, data_y, input_size)
-    history = model.fit(x, y, batch_size = 64, epochs = 50, shuffle = True, validation_split=0.2)
-    model.save(modelName + '.h5')
+    best_callback = [callbacks.ModelCheckpoint(filepath= modelName + '.h5', monitor='val_accuracy', save_best_only = True, mode='max')]
+
+    history = model.fit(x, y, batch_size = 128, epochs = 500, validation_split=0.2, shuffle=True, callbacks = best_callback)
+
     print('Model has been saved in the supplied directory as ' + modelName + '.h5')
 
 def data_org(data_x, data_y, size_selection):
