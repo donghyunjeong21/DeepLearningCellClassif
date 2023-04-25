781b92a0b53666a962ade12551dfcd44e1e1e762
donghyunjeong21 <34069123+donghyunjeong21@users.noreply.github.com>
2023-04-25 15:49:05 -0400

    Update generate_model_from_presegments.py

--- testing_codes/generate_model_from_presegments.py
+++ testing_codes/generate_model_from_presegments.py
@@ -12,17 +12,16 @@ from skimage.transform import resize
 import torch
 import cv2
 
-num_of_cells = 10502
-threshold = 1000
+
 bf_imgs = []
-for i in range(num_of_cells):
+for i in range(7974):
     filenames = str(i) + '_bf.tif'
     bf_imgs.append(io.imread(filenames))
 input_size = bf_imgs[0].shape[0]
 labels = np.loadtxt('labels.txt',delimiter = ',')
 print(labels)
 
-bin_label = np.where(labels > threshold, 1, 0)
+bin_label = np.where(labels > 1000, 1, 0)
 
 def init_model(input_size, threshold):
     if threshold == -1:
@@ -38,16 +37,19 @@ def init_model(input_size, threshold):
 
     model = models.Sequential()
     model.add(layers.InputLayer(input_shape=(input_size, input_size, 1)))
-    model.add(layers.Conv2D(64, kernel_size = (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D(pool_size = 2))
-    model.add(layers.Conv2D(64, kernel_size = (3,3), activation='relu'))
-    model.add(layers.MaxPooling2D(pool_size = 2))
-    model.add(layers.Conv2D(128, kernel_size = (5,5), activation='relu'))
-    model.add(layers.GlobalMaxPooling2D())
-    model.add(layers.Dense(256, kernel_initializer=initializer, activation = 'relu'))
-    # model.add(layers.Dropout(0.2))
+    model.add(layers.Conv2D(64, kernel_size = (3,3), strides = 2, activation='relu'))
+    model.add(layers.BatchNormalization())
+    # model.add(layers.MaxPooling2D(pool_size = 2))
+    # model.add(layers.Conv2D(64, kernel_size = (3,3), strides = 2, activation='relu'))
+    # model.add(layers.MaxPooling2D(pool_size = 2))
+    model.add(layers.Conv2D(128, kernel_size = (5,5), strides=2, activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPool2D(pool_size=(3,3)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(4096, kernel_initializer=initializer, activation = 'relu'))
+    model.add(layers.Dropout(0.5))
     # model.add(layers.Dense(128, kernel_initializer=initializer, activation = 'relu'))
-    model.add(layers.Dense(64, kernel_initializer=initializer, activation = 'relu'))
+    model.add(layers.Dense(2048, kernel_initializer=initializer, activation = 'relu'))
     # model.add(layers.Dense(32, kernel_initializer=initializer, activation = 'relu'))
     model.add(layers.Dense(1, activation = fin_activation))
     opt = optimizers.Adam(learning_rate=0.00001)
@@ -59,13 +61,35 @@ def fit_model(model, data_x, data_y, input_size, modelName):
     x,y = data_org(data_x, data_y, input_size)
     best_callback = [callbacks.ModelCheckpoint(filepath= modelName + '.h5', monitor='val_accuracy', save_best_only = True, mode='max')]
 
-    history = model.fit(x, y, batch_size = 16, epochs = 500, validation_split=0.2, shuffle=True, callbacks = best_callback)
+    history = model.fit(x, y, batch_size = 16, epochs = 100, validation_split=0.2, shuffle=True, callbacks = best_callback)
 
     print('Model has been saved in the supplied directory as ' + modelName + '.h5')
+    return history
 
 def data_org(data_x, data_y, size_selection):
     data_x = np.stack(data_x,axis=0)
     x = data_x.reshape(-1, size_selection, size_selection, 1)
     return x,data_y
-model = init_model(input_size, 50)
-fit_model(model, bf_imgs, bin_label, input_size, 'test')
+model = init_model(input_size, 1000)
+history = fit_model(model, bf_imgs, bin_label, input_size, 'test')
+
+def plot_train_curves(history):
+  accuracy = history.history["accuracy"]
+  val_accuracy = history.history["val_accuracy"]
+  loss = history.history["loss"]
+  val_loss = history.history["val_loss"]
+  epochs = range(1, len(accuracy) + 1)
+
+  plt.plot(epochs, accuracy, "b--", label="Training accuracy")
+  plt.plot(epochs, val_accuracy, "b", label="Validation accuracy")
+  plt.title("Training and Validation Accuracy")
+  plt.legend()
+  plt.figure()
+
+  plt.plot(epochs, loss, "b--", label = "Training loss")
+  plt.plot(epochs,val_loss, "b", label = "Validation loss")
+  plt.title("Training and Validation Loss")
+  plt.legend()
+  plt.show()
+
+plot_train_curves(history)
